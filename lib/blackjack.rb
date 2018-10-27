53e63a20270807c296c2ad7c8a3a784f19743131
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-27 10:17:29 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,11 +15,13 @@ end
 
 def prompt_user
   # code #prompt_user here
-  puts 
+  puts "Type 'h' to hit or 's' to stay"
  end 
  
 def get_user_input
   # code #get_user_input here
+  input = gets.chomp 
+  input 
 end
 
 def end_game
